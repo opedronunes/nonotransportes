0e707983539a57ad82a4b215712e8f7e9fb1071f
Pedro Nunes <81447137+PedroNunes-Dev@users.noreply.github.com>
2021-10-14 20:34:35 -0300

    Add files via upload

---
+++ NONO/assets/js/scrollreveal.js
@@ -0,0 +1,57 @@
+window.sr = ScrollReveal()
+
+  sr.reveal('.navbar', {
+    origin: 'bottom',
+    distance: '-100px',
+    duration: 3000
+  })
+
+  sr.reveal('.carousel', {
+    origin: 'bottom',
+    distance: '-100px',
+    duration: 3000
+  })
+
+  sr.reveal('.text_carousel', {
+    origin: 'right',
+    distance: '600px',
+    duration: 3000
+  })
+
+  sr.reveal('.section_content', {
+    origin: 'bottom',
+    distance: '100px',
+    duration: 3000
+  })
+
+  sr.reveal('.map', {
+    origin: 'left',
+    distance: '600px',
+    duration: 3000
+  })
+  
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '50px',
+    duration: 800,
+    reset: false
+  });
+
+  ScrollReveal().reveal('.section_content', { delay: 100 });
+  ScrollReveal().reveal('.img-form', { delay: 100 });
+  ScrollReveal().reveal('.form-group', { delay: 100 });
+  ScrollReveal().reveal('.article_news p', { delay: 100 });
+  */
