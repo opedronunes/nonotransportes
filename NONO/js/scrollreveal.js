df886cc7aa285e950f9341c58d2a959716774a0b
Pedro Nunes <81447137+PedroNunes-Dev@users.noreply.github.com>
2021-11-21 14:12:41 -0300

    atualização do menu mobile

---
+++ NONO/js/scrollreveal.js
@@ -0,0 +1,20 @@
+window.sr = ScrollReveal()
+
+  sr.reveal('.text_carousel', {
+    origin: 'right',
+    distance: '600px',
+    duration: 3000
+  })
+
+  sr.reveal('.section_content', {
+    origin: 'bottom',
+    distance: '100px',
+    duration: 3000
+  })
+
+  sr.reveal('.map', {
+    origin: 'left',
+    distance: '600px',
+    duration: 3000
+  })
+  
