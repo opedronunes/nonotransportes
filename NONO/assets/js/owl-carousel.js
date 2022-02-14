879cea7534e707e10087c558227224f802c32cc9
Pedro Nunes <adm.pedronunes@hotmail.com>
2022-02-14 13:18:08 -0300

    att owl-carousel and map

--- NONO/assets/js/owl-carousel.js
+++ NONO/assets/js/owl-carousel.js
@@ -2,16 +2,32 @@
  * CHAMADA DA FUNCTION
  */
 $(document).ready(function(){
-    $('.owl-carousel').owlCarousel();
-    });
+    /**
+     * CONFIG CAROUSEL
+     */
+    $('#client').owlCarousel({
+        loop:true,
+        margin:5,
+        autoplay:true,
+        autoplayTimeout:3000,
+        responsive:{
+            0:{
+                items: 3
+            },
+            500:{
+                items:5
+            }
+        }
+    })
+
+    $('.equipamentos').owlCarousel({
+        loop:true,
+        margin:5,
+        autoWidth:true,
+        autoplay:true,
+        autoplayTimeout:5000
+    })    
+
+});
     
-/**
- * CONFIG CAROUSEL
- */
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:5,
-    autoWidth:true,
-    autoplay:true,
-    autoplayTimeout:5000
-})
+
